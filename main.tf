d8e4955ca4769bbd8cd38fad9ce492ea4c56abbc
vinicius.sfernandes <vinicius.sfernandes@linx.com.br>
2023-02-23 12:03:28 -0300

    remove names

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ data "azurerm_client_config" "current" {
 }
 
 resource "azurerm_resource_group" "devops" {
-    name = "sapi-devops"
-    location = "East us"
+    name = "${var.product}-devops"
+    location = "eastus"
 }
