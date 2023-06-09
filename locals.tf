134bc131febed1be4d504dba8faa048eaf1f9d26
Kuno <gustavo.kuno@linx.com.br>
2023-06-09 08:04:36 -0300

    iac: new file locals.tf

---
+++ locals.tf
@@ -0,0 +1,27 @@
+locals {
+  main_tags = {
+    environment = var.environment
+    product     = var.product
+    department  = "TBD"
+    source      = "Terraform"
+    responsible = "Team"
+  }
+}
+
+locals {
+  keyvault_role_assignments = [
+    "Key Vault Administrator"
+  ]
+  service_principals = [
+    "${var.sp_client_id}",
+    "${data.azurerm_client_config.current.object_id}"
+  ]
+  keyvault_role_service_principal_assignment = distinct(flatten([
+    for role in local.keyvault_role_assignments : [
+      for principal in local.service_principals : {
+        principal = principal
+        role      = role
+      }
+    ]
+  ]))
+}
--- main.tf
+++ main.tf
@@ -19,33 +19,8 @@ provider "azurerm" {
 }
 
 
-locals {
-  main_tags = {
-    environment = var.environment
-    product     = var.product
-    department  = "TBD"
-    source      = "Terraform"
-    responsible = "Team"
-  }
-}
 
-locals {
-  keyvault_role_assignments = [
-    "Key Vault Administrator", "Key Vault Secrets Officer", "Key Vault Reader", "Key Vault Secrets User"
-  ]
-  service_principals = [
-    "${var.sp_client_id}",
-    "${data.azurerm_client_config.current.object_id}"
-  ]
-  keyvault_role_service_principal_assignment = distinct(flatten([
-    for role in local.keyvault_role_assignments : [
-      for principal in local.service_principals : {
-        principal = principal
-        role      = role
-      }
-    ]
-  ]))
-}
+
 
 data "azurerm_client_config" "current" {}
 
